ee14b4a779a75343ba19d76e49a7d1425403e1a2
Pete Nelson <nelsonp@aetna.com>
2019-11-03 10:42:27 -0700

    build graphql ts from graphql schema

---
+++ src/queries/skills.ts
@@ -0,0 +1,9 @@
+import { gql } from 'apollo-boost'
+export default gql`
+  query Skills($type: SkillType) {
+    skills(type: $type) {
+      name
+      type   
+    }
+  }
+`
---
+++ src/types/graphql-global-types.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export enum SkillType {
+  DATABASE = "DATABASE",
+  DESIGN = "DESIGN",
+  LANGUAGE = "LANGUAGE",
+  SERVER = "SERVER",
+  TESTING = "TESTING",
+  UI = "UI",
+  UI_FRAMEWORK = "UI_FRAMEWORK",
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
