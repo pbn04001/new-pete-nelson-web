e6ee619b9d48dc527cd376bc4086927ded29b5bf
Pete Nelson <nelsonp@aetna.com>
2019-11-07 11:15:19 -0700

    Polling working

--- src/views/ProvidersView.tsx
+++ src/views/ProvidersView.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withApollo } from 'react-apollo'
 import { ApolloClient, InMemoryCache } from 'apollo-boost';
 import { useQuery } from 'react-apollo-hooks';
@@ -7,10 +7,7 @@ import {Providers as ProvidersGQL, Providers_providers_providers } from "../quer
 import {ProviderPrices, ProviderPrices_providerPrices } from "../queries/types/ProviderPrices";
 import providerPricesQuery from "../queries/providerPrices";
 
-const renderPrice = (provider: Providers_providers_providers, prices?: ProviderPrices_providerPrices[]) => {
-  if (!prices) return null
-
-  const price = prices.find(price => price.id === provider.id)?.price
+const renderPrice = (price?: number) => {
   if (price) {
     return <div>${price}</div>
   }
@@ -24,8 +21,33 @@ type ProvidersViewProps = {
 const ProvidersView: React.FC<ProvidersViewProps> = ({
   client,
 }: ProvidersViewProps) => {
-  const { data: providers, loading: loading } = useQuery<ProvidersGQL>(providerQuery, {client })
-  const { data: prices } = useQuery<ProviderPrices>(providerPricesQuery, { client })
+  const [count, setCount ] = useState(0)
+  const [prices, updatePrices ] = useState<{ [key: number]: number }>({})
+  const { data: providers, loading } = useQuery<ProvidersGQL>(providerQuery, { client })
+  const priceValues = useQuery<ProviderPrices>(providerPricesQuery, {
+    client,
+    pollInterval: 1000
+  })
+  const { data: newPrices, stopPolling } = priceValues;
+
+  useEffect(() => {
+    console.log(`Polled ${count} times`)
+    if (count >= 20) {
+      stopPolling()
+    } else {
+      setCount(count + 1)
+    }
+
+    if (newPrices?.providerPrices && newPrices.providerPrices.length > 0) {
+      const updatedPrices: { [key: number]: number } = {
+        ...prices,
+      }
+      for (const price of newPrices.providerPrices) {
+        updatedPrices[price.id] = price.price
+      }
+      updatePrices(updatedPrices)
+    }
+  }, [priceValues])
 
   if (loading) return <div>Loading...</div>
 
@@ -36,7 +58,7 @@ const ProvidersView: React.FC<ProvidersViewProps> = ({
         <div key={provider.id}>
           <div>{provider.name}</div>
           <div>{provider.location}</div>
-          {renderPrice(provider, prices?.providerPrices)}
+          {renderPrice(prices[provider.id])}
           <br/>
         </div>
       ))}
