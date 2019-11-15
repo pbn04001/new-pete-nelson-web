d97b5b0205e7168ef75c776182a8b624a368b373
Pete Nelson <nelsonp@aetna.com>
2019-11-14 19:38:58 -0700

    babel working

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,39 @@
-const { useBabelRc, override } = require('customize-cra')
+const path = require('path')
 
-module.exports = override(
-  useBabelRc()
-);
+module.exports = {
+  webpack: function(config, env) {
+
+    const src = path.join(path.resolve(__dirname, '.'), '/src/')
+
+    config.module.rules.push({
+      // JavaScript / JSON
+      test    : /\.(js|jsx|ts|tsx)$/,
+      include : [
+        src,
+      ],
+      exclude: /src\/assets/,
+      use : [{
+        loader  : 'babel-loader',
+        options : {
+          babelrc        : false,
+          cacheDirectory : true,
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+            '@babel/preset-typescript'
+          ],
+          plugins: [
+            '@babel/plugin-proposal-optional-chaining',
+            '@babel/plugin-proposal-nullish-coalescing-operator'
+          ],
+          ignore: [
+            'node_modules',
+            'src/assets/*'
+          ]
+        }
+      }]
+    })
+
+    return config;
+  }
+}
